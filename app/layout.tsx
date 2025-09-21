47d75d05041bef6537affe6bde730635b49a84fe
Brandon Reynolds <brandon-reynolds@outlook.com>
2025-09-21 14:05:55 +1000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import Link from "next/link";
-import NavAuth from "@/components/NavAuth";
+import NavAuth from "../components/NavAuth";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
